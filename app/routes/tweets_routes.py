e15fdc538ddcac80c6257822e7727266be38e2e8
Johan Mazorra <johanmazorra@Claudias-MacBook-Air-3.local>
2020-04-22 18:49:56 -0400

    Update

--- app/routes/tweets_routes.py
+++ app/routes/tweets_routes.py
@@ -1,20 +1,26 @@
-from flask import Blueprint, jsonify, request, render_template , flash, redirect
-from app.models import User, Tweet, parse_records, db
+from flask import Blueprint, jsonify, render_template
+from app.models import User, Tweet, db, parse_records
 from app.services.twitter_service import twitter_api
-from app.services.basilica_service import basilica_api_client
+from app.services.basilica_service import connection as basilica_connection
 
-tweet_routes = Blueprint("tweet_routes", __name__)
+tweets_routes = Blueprint("tweets_routes", __name__)
 
-@tweet_routes.route("/users/<screen_name>")
-def get_user(screen_name=None):
+@tweets_routes.route("/users/<screen_name>/fetch")
+def fetch_user_data(screen_name=None):
     print(screen_name)
 
-    api = twitter_api()
-
+    api = api_client()
     twitter_user = api.get_user(screen_name)
-    statuses = api.user_timeline(screen_name, tweet_mode="extended", count=150, exclude_replies=True, include_rts=False)
-    #print("STATUSES COUNT:", len(statuses))
-    #return jsonify({"user": user._json, "tweets": [s._json for s in statuses]})
+    statuses = api.user_timeline(screen_name, tweet_mode="extended", count=150)
+    print("STATUSES COUNT:", len(statuses))
+
+    #new_book = Book(title=request.form["book_title"], author_id=request.form["author_name"])
+    #db.session.add(new_book)
+    #db.session.commit()
+
+    #
+    # STORE USER
+    #
 
     # get existing user from the db or initialize a new one:
     db_user = User.query.get(twitter_user.id) or User(id=twitter_user.id)
@@ -24,70 +30,50 @@ def get_user(screen_name=None):
     db_user.followers_count = twitter_user.followers_count
     db.session.add(db_user)
     db.session.commit()
-    #return "OK"
     #breakpoint()
 
-    basilica_api = basilica_api_client()
+    #
+    # STORE TWEETS
+    #
 
     all_tweet_texts = [status.full_text for status in statuses]
-    embeddings = list(basilica_api.embed_sentences(all_tweet_texts, model="twitter"))
-    #print("NUMBER OF EMBEDDINGS", len(embeddings))
+    embeddings = list(basilica_connection.embed_sentences(all_tweet_texts, model="twitter"))
+    print("NUMBER OF EMBEDDINGS", len(embeddings))
 
     # TODO: explore using the zip() function maybe...
     counter = 0
     for status in statuses:
-        #print(status.full_text)
-        #print("----")
+        print(status.full_text)
+        print("----")
         #print(dir(status))
         # get existing tweet from the db or initialize a new one:
         db_tweet = Tweet.query.get(status.id) or Tweet(id=status.id)
         db_tweet.user_id = status.author.id # or db_user.id
         db_tweet.full_text = status.full_text
-        #embedding = basilica_client.embed_sentence(status.full_text, model="twitter") # todo: prefer to make a single request to basilica with all the tweet texts, instead of a request per tweet
+        #embedding = basilica_connection.embed_sentence(status.full_text, model="twitter") # todo: prefer to make a single request to basilica with all the tweet texts, instead of a request per tweet
         embedding = embeddings[counter]
-        #print(len(embedding))
+        print(len(embedding))
         db_tweet.embedding = embedding
         db.session.add(db_tweet)
         counter+=1
     db.session.commit()
-    #breakpoint()
-    return "OK"
 
-"""@tweet_routes.route("/tweets.json")
-def list_tweets():
-    #tweets = [
-    #    {"id": 1, "title": "Book 1"},
-    #    {"id": 2, "title": "Book 2"},
-    #    {"id": 3, "title": "Book 3"},
-    #]
-    tweet_records = Tweet.query.all()
-    print(tweet_records)
-    tweets = parsed_records(tweet_records)
-    return jsonify(tweets)
-@tweet_routes.route("/tweets")
-def list_tweets_for_humans():
-    #tweets = [
-    #    {"id": 1, "title": "Book 1"},
-    #    {"id": 2, "title": "Book 2"},
-    #    {"id": 3, "title": "Book 3"},
-    #]
-    #return render_template("tweets.html", message="Here's some tweets", tweets=tweets)
-    tweet_records = Tweet.query.all()
-    print(tweet_records)
-    return render_template("tweets.html", message="Here's some tweets", tweets=tweet_records)
-@tweet_routes.route("/tweets/new")
-def new_tweet():
-    return render_template("new_tweet.html")
-@tweet_routes.route("/tweets/create", methods=["POST"])
-def create_tweet():
-    print("FORM DATA:", dict(request.form))
-    # todo: store in database
-    new_tweet = Tweet(text=request.form["tweet_text"], author_id=request.form["tweeter"])
-    db.session.add(new_tweet)
-    db.session.commit()
-    #return jsonify({
-    #    "message": "TWEET CREATED OK (TODO)",
-    #    "tweet": dict(request.form)
-    #})
-    flash(f"Tweet '{new_tweet.text}' created successfully!", "success")
-    return redirect(f"/tweets")"""
+    return "OK"
+    #return render_template("user.html", user=db_user, tweets=statuses) # tweets=db_tweets
+
+@tweets_routes.route("/users")
+def list_users_human_friendly():
+    db_users = User.query.all()
+    return render_template("users.html", users=db_users)
+
+@tweets_routes.route("/users.json")
+def list_users():
+    db_users = User.query.all()
+    users_response = parse_records(db_users)
+    return jsonify(users_response)
+
+@tweets_routes.route("/users/<screen_name>")
+def get_user(screen_name=None):
+    print(screen_name)
+    db_user = User.query.filter(User.screen_name == screen_name).one()
+    return render_template("user.html", user=db_user, tweets=db_user.tweets)
